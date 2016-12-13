3e2bd402638bbe865a13cb012dcbb3967bf78ba0
Matis Kleidman <matis@socialcodeinc.com>
2016-12-13 09:30:22 -0500

    move some settings into app. change admin slightly

--- planner/settings.py
+++ planner/settings.py
@@ -1,3 +1,28 @@
 """
 Planner app settings
 """
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'planner'
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
