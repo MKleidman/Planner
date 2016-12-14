5bc754c501b38c85d80fb50961ff1783b54f59f8
Matis Kleidman <matis@socialcodeinc.com>
2016-12-14 15:13:58 -0500

    add more settings and Makefile

--- planner/settings.py
+++ planner/settings.py
@@ -29,3 +29,11 @@ TEMPLATES = [
 ]
 
 ROOT_URLCONF = 'planner.urls'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
