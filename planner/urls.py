62a4d99aa5339f6ef9fdba5bc4cbeb9f944c0f8c
Matis Kleidman <matis@socialcodeinc.com>
2016-12-15 13:33:49 -0500

    add health check and beginning of google login

--- planner/urls.py
+++ planner/urls.py
@@ -1,7 +1,18 @@
+from django.http import HttpResponse
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render_to_response
 from rest_framework import routers
 from planner import views
+
+def login_view(request):
+	return render_to_response('google-login.html')
+
+def health_check(request):
+    """Health check for determining if the server is available in an Amazon Elastic Load Balancer."""
+    response_content = '<html><body>OK</body></html>'
+    return HttpResponse(response_content, content_type='text/html')
 
 admin.autodiscover()
 
@@ -16,4 +27,6 @@ urlpatterns = [
 	url(r'^api-auth/', include('rest_framework.urls')),
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
+	url(r'^login/$', login_view),
+	url(r'^/$', health_check)
 ]
